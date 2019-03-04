16db43020f71bd2283f90ed9583a1cc3ce0c4ee0
Bryan Kribbs <bkribbs15@gmail.com>
2019-03-04 15:05:51 -0600

    files fixes

---
+++ container-ms/ContainerProducer.py
@@ -0,0 +1,29 @@
+import json
+from confluent_kafka import KafkaError, Producer
+
+
+KAFKA_BROKERS=os.environ['KAFKA_BROKERS']
+
+containerProducer = Producer({
+    'bootstrap.servers': KAFKA_BROKERS
+})
+
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+
+def publishEvent():
+    data = {"timestamp": int(time.time()), "type": "OrderContainerAllocated", "version": "1", "payload": {"containerID": "c10", "orderID": orderID}}
+    dataStr = json.dumps(data)
+    containerProducer.produce('orders', dataStr.encode('utf-8'), callback=delivery_report)
+    containerProducer.flush()
+
+
+
+container.flush()
