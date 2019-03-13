268edd6761250a7d9b97a1c983756327c031c1ea
Bryan Kribbs <bkribbs15@gmail.com>
2019-03-13 17:10:36 -0500

    example data push working

--- container-ms/scripts/ContainerProducer.py
+++ container-ms/scripts/ContainerProducer.py
@@ -8,14 +8,14 @@ from generateData import buildJSON
 #KAFKA_BROKERS = os.environ['KAFKA_BROKERS']
 
 containerProducer = Producer({
-    'bootstrap.servers': 'localhost:2181'
+    'bootstrap.servers': 'localhost:9092'
 })
 
 data = buildJSON(os.path.join(os.path.dirname(os.path.dirname(sys.path[0])),'data','containerData.csv'))
 print('Data', json.dumps(data, indent=4, sort_keys=True))
 
-class KafkaPublish:
-    def delivery_report(err, msg):
+class ContainerPublish:
+    def delivery_report(this,err, msg):
          """ Called once for each message produced to indicate delivery result.
              Triggered by poll() or flush(). """
          if err is not None:
@@ -23,8 +23,18 @@ class KafkaPublish:
          else:
              print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
-    def publishEvent(data):
-        print('pE')
+    def publishEvent(self,data):
+        print('PED', data)
+        print('PUBLISH')
         dataStr = json.dumps(data)
-        containerProducer.produce('ContainerMetrics', dataStr.encode('utf-8'), callback=delivery_report)
+        containerProducer.produce('ContainerMetrics', dataStr.encode('utf-8'), callback=ContainerPublisher.delivery_report)
         containerProducer.flush()
+
+
+ContainerPublisher = ContainerPublish()
+
+data = buildJSON(os.path.join(os.path.dirname(os.path.dirname(sys.path[0])),'data','containerData.csv'))
+print('Data', json.dumps(data, indent=4, sort_keys=True))
+print(data[0])
+
+ContainerPublisher.publishEvent(data[0])
