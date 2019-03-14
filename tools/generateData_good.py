8487cc99d25b072e8e37f9a44005aadd898e4a5b
Shilpi Bhattacharyya <shilpibhattacharyya@Shilpis-MacBook-Pro.local>
2019-03-13 22:46:11 -0400

    updating data generation script files..

--- tools/generateData_good.py
+++ tools/generateData_good.py
@@ -3,6 +3,7 @@ import json
 from random import gauss
 import random
 import datetime
+import numpy as np
 
 containerData = []
 def buildJSON(csvfile):
@@ -20,15 +21,15 @@ with open('container_matrix.csv', mode='w') as container_file:
     container_writer.writerow(['Timestamp', 'ID', 'Temperature(celcius)', 'Target_Temperature(celcius)', 'Amp', 'CumulativePowerConsumption', 'ContentType', 'Humidity', 'CO2', 'Door_Open', 
     'Under Maintainence', 'Defrost_Cycle'])
 
-    #faulty sensor data
+    #good sensor data
     id = random.randint(1001,2000)
     Today= datetime.datetime.today()
     date_list = [Today + datetime.timedelta(minutes=15*x) for x in range(0, 10)]
-    print date_list
+
     for i in range(0, 10):
 
         timestamp = date_list[i].strftime('%Y-%m-%d T%H:%M Z')
-    	container_writer.writerow([timestamp, id, gauss(5.0,5.0), 4.4, gauss(2.5,1.0), gauss(10.0,2.0), random.randint(1,5), 
+    	container_writer.writerow([timestamp, id, 2.4, 4.4, gauss(2.5,1.0), gauss(10.0,2.0), random.randint(1,5), 
         gauss(10.5, 5.5), gauss(10.5, 5.0), 'false', 'false', 6])
 
 print(buildJSON('container_matrix.csv'))
