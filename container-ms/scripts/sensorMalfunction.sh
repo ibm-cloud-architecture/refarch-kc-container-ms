46aa0c004191719bd0a320ad47f9ef6f3160859b
Bryan <bkribbs15@gmail.com>
2019-03-19 17:04:48 -0500

    updated scripts

--- container-ms/scripts/sensorMalfunction.sh
+++ container-ms/scripts/sensorMalfunction.sh
@@ -5,12 +5,16 @@ python $toolsDIR/generateData_sensor_malfunction.py
 echo '\n Done Generating \n'
 
 #Publish to Kafka 
+echo 'Publish Kafka \n'
 dataDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../../data && pwd )
 dataFile="$dataDIR/container_matrix_sensor_malfunction.csv"
 echo $dataFile
 python3 containerProducer.py $dataFile
+echo 'Kafka Done \n'
 
 #Test the Model
-#ipython nbconvert --to python predictMaintainence.ipynb
-#data = pd.read_csv('../data/container_matrix_door_open.csv', delimiter=",")
-#ipython nbconvert --to python ../../predictiveModel/predictMaintainence.ipynb
+echo 'Testing Model'
+modelDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../../predictiveModel && pwd )
+jupyter nbconvert --to python "$modelDIR/predictMaintainence.ipynb"
+data=pd.read_csv("$dataDIR/container_matrix_sensor_malfunction.csv", delimiter=",")
+echo 'Model Tested'
---
+++ predictiveModel/predictMaintainence.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+from sklearn import metrics
+
+
+# In[19]:
+
+
+data = pd.read_csv('../data/container_matrix_door_open.csv', delimiter=",")
+data.head()
+
+
+# In[20]:
+
+
+# create a Python list of feature names
+feature_cols = ['Temperature(celsius)', 'Door_Open','Target_Temperature(celsius)','Amp','ContentType','CumulativePowerConsumption'
+                          ,'Humidity','CO2','Defrost_Cycle']
+
+# use the list to select a subset of the original DataFrame
+X = data[feature_cols]
+y = data['Maintainence_Required']
+
+# print the first 5 rows
+X.head()
+
+
+# In[24]:
+
+
+import pickle
+
+#loading a model from a file called model.pkl
+model = pickle.load(open("model.pkl","r"))
+
+
+# In[27]:
+
+
+# make predictions on the input set
+y_pred = model.predict(X)
+print y_pred
+#y_pred.shape
+print(np.sqrt(metrics.mean_squared_error(y, y_pred)))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
