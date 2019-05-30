c76e7e79285fc4072aeebef7298bccdbf60699a1
Jerome Boyer <boyerje@us.ibm.com>
2019-05-29 20:50:08 -0700

    fix a strange pom delete

--- SpringContainerMS/scripts/runDocker.sh
+++ SpringContainerMS/scripts/runDocker.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-root_folder=$(cd $(dirname $0); cd ..; pwd)
-. ../scripts/setenv.sh LOCAL
+
+
+if [[ $PWD = */scripts ]]; then
+ cd ..
+fi
+if [[ $# -eq 0 ]];then
+  kcenv="LOCAL"
+else
+  kcenv=$1
+fi
+
+msname="springcontainerms"
+ns="greencompute"
+chart=$(ls ./chart/| grep $msname)
+kname="kc-"$chart
+source ../../refarch-kc/scripts/setenv.sh $kcenv
 echo $kname
 docker rm $kname
 # docker run  --name $kname -e POSTGRESQL_URL=$POSTGRESQL_URL -e  POSTGRESQL_CA_PEM=$POSTGRESQL_CA_PEM -e POSTGRESQL_USER=$POSTGRESQL_USER -e POSTGRESQL_PWD=$POSTGRESQL_PWD  -p 8080:8080 ibmcase/$kname
 
-docker run -ti --name $kname -e POSTGRESQL_URL=$POSTGRESQL_URL -e POSTGRESQL_CA_PEM="${POSTGRESQL_CA_PEM}" -e POSTGRESQL_USER=$POSTGRESQL_USER -e POSTGRESQL_PWD=$POSTGRESQL_PWD -p 8080:8080 ibmcase/$kname bash
+docker run --name $kname -e KAFKA_BROKERS=$KAFKA_BROKERS -e POSTGRESQL_URL=$POSTGRESQL_URL -e POSTGRESQL_CA_PEM=$POSTGRESQL_CA_PEM -e POSTGRESQL_USER=$POSTGRESQL_USER -e POSTGRESQL_PWD=$POSTGRESQL_PWD -p 8080:8080 -ti  ibmcase/$kname 
--- SpringContainerMS/scripts/startup.sh
+++ SpringContainerMS/scripts/startup.sh
@@ -3,10 +3,13 @@ root_folder=$(cd $(dirname $0); cd ..; pwd)
 
 scripts/add_certificates.sh
 echo $JAVA_HOME
+echo $POSTGRESQL_URL
+echo $POSTGRESQL_CA_PEM
+echo $KAFKA_BROKERS
 export JKS_LOCATION=${JAVA_HOME}"/jre/lib/security/cacerts"
 
 # Set basic java options
-# export JAVA_OPTS="${JAVA_OPTS} -Djavax.net.ssl.trustStore=${JAVA_HOME}/lib/security/cacerts -Djavax.net.ssl.trustStorePassword=changeit"
-export JAVA_OPTS="${JAVA_OPTS} -Djavax.net.ssl.trustStore=clienttruststore -Djavax.net.ssl.trustStorePassword=changeit"
+export JAVA_OPTS="${JAVA_OPTS} -Djavax.net.ssl.trustStore=${JAVA_HOME}/lib/security/cacerts -Djavax.net.ssl.trustStorePassword=changeit"
+# export JAVA_OPTS="${JAVA_OPTS} -Djavax.net.ssl.trustStore=clienttruststore -Djavax.net.ssl.trustStorePassword=changeit"
 echo ${JAVA_OPTS}
 java ${JAVA_OPTS} -jar  ./app.jar 
