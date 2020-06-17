7d2bae5aab81fcb72371fadeb3058a7ac1a0b745
David Jones <djones6@uk.ibm.com>
2020-06-17 18:50:09 +0100

    Update service name to match Appsody (#50)
    * Update service name to match Appsody
    
    * Remove files not needed with Appsody dev env

--- scripts/os-setenv.sh
+++
@@ -1,7 +0,0 @@
-source ../../../refarch-kc/scripts/setenv.sh IBMCLOUD
-oc set env dc/springcontainermgr KAFKA_BROKERS=$KAFKA_BROKERS
-oc set env dc/springcontainermgr KAFKA_ENV=$KAFKA_ENV
-oc set env dc/springcontainermgr KAFKA_APIKEY=$KAFKA_APIKEY
-oc set env dc/springcontainermgr POSTGRESQL_URL=$POSTGRESQL_URL
-oc set env dc/springcontainermgr POSTGRESQL_USER=$POSTGRESQL_USER
-oc set env dc/springcontainermgr POSTGRESQL_PWD=$POSTGRESQL_PWD
